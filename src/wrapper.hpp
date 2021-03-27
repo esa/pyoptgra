8eaff8d59c7726da57dac6e8eff1e98fbe79ea16
Moritz v. Looz <Moritz.v.Looz@esa.int>
2021-03-27 17:31:44 +0100

    wrapped oginit and ogclos in raii struct

--- src/wrapper.hpp
+++ src/wrapper.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <stdexcept>
 #include <vector>
 #include <tuple>
 
@@ -16,12 +17,19 @@ extern"C" {
 
 namespace optgra {
 
+    typedef void (*fitness_callback)(double*, double*, int*);
+
+    typedef void (*gradient_callback)(double*, double*, double*);
+
 struct optgra_raii {
 
+    optgra_raii() = delete;
+    // TODO: Use a mutex to ensure that at most one object can be created concurrently
+
     optgra_raii(int num_variables, const std::vector<int> &constraint_types,
-     int difftype, std::vector<double> autodiff_deltas = {})
+     int difftype, std::vector<double> autodiff_deltas = {}) : num_variables(num_variables)
     {
-        int num_constraints = constraint_types.size();
+        num_constraints = constraint_types.size();
         if (autodiff_deltas.size() == 0) {
             autodiff_deltas = std::vector<double>(num_variables, 0.1);
         }
@@ -29,10 +37,31 @@ struct optgra_raii {
         ogderi_(&difftype, autodiff_deltas.data());
     }
 
+    std::tuple<std::vector<double>, std::vector<double>, int> exec(std::vector<double> initial_x,
+    fitness_callback fitness, gradient_callback gradient) {
+
+        if (int(initial_x.size()) != num_variables) {
+            throw(std::invalid_argument("Expected " + std::to_string(num_variables) + ", but got " + std::to_string(initial_x.size())));
+        }
+
+        std::vector<double> valvar(initial_x);
+        std::vector<double> valcon(num_constraints+1);
+
+        int finopt = 0;
+        int finite = 0;
+        ogexec_(valvar.data(), valcon.data(), &finopt, &finite, fitness, gradient);
+
+        return std::make_tuple(valvar, valcon, finopt);
+    }
+
     ~optgra_raii()
     {
         ogclos_();
     }
+
+private:
+    int num_variables;
+    int num_constraints;
 };
 
 template<class F, class G>
@@ -56,14 +85,10 @@ std::tuple<std::vector<double>, std::vector<double>, int> optimize(const std::ve
 
     optgra_raii raii_object = optgra_raii(num_variables, constraint_types, difftype, autodiff_deltas);
 
-    int finopt = 0;
-    int finite = 0;
-    ogexec_(valvar.data(), valcon.data(), &finopt, &finite, fitness, gradient);
-    return std::make_tuple(valvar, valcon, finopt);
+    return raii_object.exec(initial_x, fitness, gradient);
 }
 
 }
-
 
 /**
  * oginit.F : Allocates and zeroes vectors, sets parameter values in common block to hardcoded defaults
