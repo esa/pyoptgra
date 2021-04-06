9737e9bb753accbae13db75492970412ae954c19
Moritz v. Looz <Moritz.v.Looz@esa.int>
2021-04-07 00:38:10 +0200

    added rudimentary structure for python packaging

---
+++ setup.py
@@ -0,0 +1,5 @@
+import sys
+
+from skbuild import setup
+
+setup(name="pyoptgra", packages=["pyoptgra"], setup_requires=['cmake', 'ninja'], cmake_install_dir='pyoptgra/core')
---
+++ tests/test.py
@@ -0,0 +1,21 @@
+import pyoptgra
+import pygmo
+
+import unittest
+
+class SimplePygmoTest(unittest.TestCase):
+	# Basic test that the call works and the result changes. No constraints, not gradients.
+
+	algo = pygmo.algorithm(pyoptgra.optgra())
+	prob = pygmo.problem(pygmo.schwefel(30))
+	pop = pygmo.population(prob, 1)
+	previous_best = pop.champion_f
+
+	# Calling optgra
+	pop = algo.evolve(pop)
+	new_best = pop.champion_f
+
+	self.assertLess(new_best, previous_best)
+
+if __name__ == '__main__':
+    unittest.main()
