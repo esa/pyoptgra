edc7cd76be070978a30467ec5f3a696a948068e9
Moritz v. Looz <Moritz.v.Looz@esa.int>
2021-04-07 20:38:59 +0200

    added gradient test case

--- tests/test.py
+++ tests/test.py
@@ -3,13 +3,30 @@ import pygmo
 
 import unittest
 
-class SimplePygmoTest(unittest.TestCase):
+class pygmo_test(unittest.TestCase):
 
-	def test(self):
+	def runTest(self):
+		self.basic_no_gradient_test()
+		self.gradient_no_constraints_test()
+
+	def basic_no_gradient_test(self):
 		# Basic test that the call works and the result changes. No constraints, not gradients.
 
 		algo = pygmo.algorithm(pyoptgra.optgra())
 		prob = pygmo.problem(pygmo.schwefel(30))
+		pop = pygmo.population(prob, 1)
+		previous_best = pop.champion_f
+
+		# Calling optgra
+		pop = algo.evolve(pop)
+		new_best = pop.champion_f
+
+		self.assertLess(new_best, previous_best)
+
+	def gradient_no_constraints_test(self):
+
+		algo = pygmo.algorithm(pyoptgra.optgra())
+		prob = pygmo.problem(pygmo.rosenbrock(30))
 		pop = pygmo.population(prob, 1)
 		previous_best = pop.champion_f
 
