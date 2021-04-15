e740b9fb741f17af5006f418791866e24c6d7235
Moritz v. Looz <Moritz.v.Looz@esa.int>
2021-04-15 21:20:31 +0200

    added tests for input checks

--- tests/test.py
+++ tests/test.py
@@ -34,20 +34,93 @@ class luksan_vlcek:
 
 class pygmo_test(unittest.TestCase):
     def runTest(self):
+        self.constructor_test()
+        self.evolve_input_check_test()
         self.basic_no_gradient_test()
         self.gradient_no_constraints_test()
         self.gradient_with_constraints_test()
 
-    def basic_no_gradient_test(self):
-        # Basic test that the call works and the result changes. No constraints, not gradients.
+    def constructor_test(self):
+        # Check that invalid optimization method is rejected
+        with self.assertRaises(ValueError):
+            algo = pygmo.algorithm(pyoptgra.optgra(optimization_method=5))
 
-        algo = pygmo.algorithm(pyoptgra.optgra(derivatives_computation=2))
+        # Check that negative iteration count is rejected
+        with self.assertRaises(ValueError):
+            algo = pygmo.algorithm(pyoptgra.optgra(max_iterations=-1))
+
+        # Check that negative correction iteration count is rejected
+        with self.assertRaises(ValueError):
+            algo = pygmo.algorithm(pyoptgra.optgra(max_correction_iterations=-1))
+
+        # Check that negative distance per iteration is rejected
+        with self.assertRaises(ValueError):
+            algo = pygmo.algorithm(pyoptgra.optgra(max_distance_per_iteration=-1))
+
+        # Check that negative perturbation is rejected
+        with self.assertRaises(ValueError):
+            algo = pygmo.algorithm(
+                pyoptgra.optgra(perturbation_for_snd_order_derivatives=-1)
+            )
+
+        # Check that conflicting sizes of constraint scalings and priorities are rejected
+        with self.assertRaises(ValueError):
+            algo = pygmo.algorithm(
+                pyoptgra.optgra(
+                    convergence_thresholds=[1], constraint_priorities=[1, 2]
+                )
+            )
+
+        # Valid constructor
+        pygmo.algorithm(pyoptgra.optgra())
+
+    def evolve_input_check_test(self):
+        algo = pygmo.algorithm(pyoptgra.optgra())
         prob = pygmo.problem(pygmo.schwefel(30))
 
         # Check that empty population is rejected
         empty_pop = pygmo.population(prob, 0)
         with self.assertRaises(ValueError):
-        	empty_pop = algo.evolve(empty_pop)
+            empty_pop = algo.evolve(empty_pop)
+
+        class toy_multi_problem:
+            def __init__(self):
+                pass
+
+            def fitness(self, x):
+                return (sum(x), 1)
+
+            def get_bounds(self):
+                return ([0, 0], [1, 1])
+
+            def get_nobj(self):
+                return 2
+
+        class toy_stochastic_problem:
+            def __init__(self):
+                self.seed = 0
+
+            def fitness(self, x):
+                import random
+
+                return random.random() + x[0]
+
+            def get_bounds(self):
+                return ([0], [1])
+
+            def set_seed(self, seed):
+                self.seed = seed
+
+    def basic_no_gradient_test(self):
+        # Basic test that the call works and the result changes. No constraints, not gradients.
+
+        algo = pygmo.algorithm(pyoptgra.optgra())
+        prob = pygmo.problem(pygmo.schwefel(30))
+
+        # Check that empty population is rejected
+        empty_pop = pygmo.population(prob, 0)
+        with self.assertRaises(ValueError):
+            empty_pop = algo.evolve(empty_pop)
 
         # Prepare normal population
         pop = pygmo.population(prob, 1)
