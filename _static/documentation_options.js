2a38d3c413907c7a897fd233a67c8bb19853ae09
CircleCI <moritz@vlooz.de>
2021-12-10 17:07:34 +0000

    Update Sphinx documentation commit c9e267aa8ab8028e5dc756b968c261c40580b4a7 [skip ci].

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+g8c5da0d',
+    VERSION: '0.1.dev1+gc9e267a',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
