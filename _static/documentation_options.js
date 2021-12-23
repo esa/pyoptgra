c0ded57d5ebe75af32fb8ee0427c76a5be190a5d
Github Workflow <moritz@vlooz.de>
2021-12-23 16:03:00 +0000

    Update Sphinx documentation commit d7e2804787506686e48abb08641c31904e217c5f [skip ci].

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+gf9fe4ba',
+    VERSION: '0.1.dev1+gd7e2804',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
