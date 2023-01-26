02f901c58635591f27bcc7119d245fee50ffa58c
wmartens <waldemar.martens@esa.int>
2023-01-26 18:55:08 +0000

    removed check for .git directory

--- cmake/modules/Update_submodules.cmake
+++ cmake/modules/Update_submodules.cmake
@@ -1,13 +1,11 @@
 # Update git submodules as needed
 
 # The Python source distribution isn't a git checkout
-message(STATUS "Update submodules")
-if (EXISTS ${PROJECT_SOURCE_DIR}/.git)
-    message(STATUS "Performing submodule update")
-    execute_process(COMMAND git submodule update --init --recursive
-    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-    RESULT_VARIABLE GIT_SUBMOD_RESULT)
-    if(NOT GIT_SUBMOD_RESULT EQUAL "0")
-    message(FATAL_ERROR "git submodule update --init failed with ${GIT_SUBMOD_RESULT}, please checkout submodules")
-    endif()
+message(STATUS "Performing submodule update")
+execute_process(COMMAND git submodule update --init --recursive
+WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+RESULT_VARIABLE GIT_SUBMOD_RESULT)
+if(NOT GIT_SUBMOD_RESULT EQUAL "0")
+message(FATAL_ERROR "git submodule update --init failed with ${GIT_SUBMOD_RESULT}, please checkout submodules")
 endif()
+
