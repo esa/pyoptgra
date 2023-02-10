e0eb8115c2624396db32a09355158db1f6a1be07
wmartens <waldemar.martens@esa.int>
2023-02-10 13:33:24 +0000

    removed automatic versioning

---
+++ pyoptgra/_about.py
@@ -0,0 +1,3 @@
+# Copyright (C) 2021 ESA, Flight Dynamics OPS-GF
+"""Version number of pyoptgra package"""
+__version__ = "0.1.8"
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@
 # You may choose to license and use it under version 3 of the
 # GNU General Public License or under the
 # ESA Software Community Licence (ESCL) 2.4 Weak Copyleft.
-# We explicitly reserve the right to release future versions of 
+# We explicitly reserve the right to release future versions of
 # Pyoptgra and Optgra under different licenses.
 # If copies of GPL3 and ESCL 2.4 were not distributed with this
 # file, you can obtain them at https://www.gnu.org/licenses/gpl-3.0.txt
@@ -16,11 +16,16 @@ import sys
 
 from skbuild import setup
 
+# load version number
+about = dict()
+exec(open("pyoptgra/_about.py").read(), about)
+version = about["__version__"]
+
 setup(
     name="pyoptgra",
-    use_scm_version= {"write_to" : "pyoptgra/_version.py"},
+    version=version,
     packages=["pyoptgra"],
-    setup_requires=["cmake", "ninja","setuptools_scm"],
+    setup_requires=["cmake", "ninja"],
     install_requires=["pygmo==2.16"],
     cmake_install_dir="pyoptgra/core",
 )
